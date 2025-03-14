d522e7ffae7a4bb0313ba48ae0c37326683ebfba
Dmitriy Kondratenko <dmitriy.kondratenko@phobosys.com>
2025-03-14 15:53:16 +0200

    Add currencies

--- src/mobile/components/SVG/currency/funds/index.js
+++ src/mobile/components/SVG/currency/funds/index.js
@@ -1,5 +1,6 @@
 import UNKNOWN_DARK from './UNKNOWN.DARK'
 import ETH_DARK from './ETH.DARK'
+import BTC_DARK from './BTC.DARK'
 import USDC_DARK from './USDC.DARK'
 import DEMO_DARK from './DEMO.DARK'
 import ORCY_DARK from './ORCY.DARK'
@@ -8,9 +9,14 @@ const FUNDS = {
   UNKNOWN_DARK,
 
   ETH_DARK,
+  WETH_DARK: ETH_DARK,
+  BTC_DARK,
+  WBTC_DARK: BTC_DARK,
   DEMO_DARK,
   ORCY_DARK,
   USDC_DARK,
+  'USDC.e_DARK': USDC_DARK,
+  USDT_DARK: USDC_DARK,
 }
 
 export function factoryFunds(currency, theme = 'DARK') {
